aafc2f03efa0bfe928af42cfd9dee98b23312b93
Pól Ua Laoínecháin <linehanp@tcd.ie>
2014-02-23 18:00:24 +0000

    Create ACeDB2JavaObjectNotFoundException.java

---
+++ ACeDB2JavaObjectNotFoundException.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2014 Kemin Zhou, Pól Ua Laoínecháin. 
+ *
+ * The original and  main author is Kemin Zhou and the original code is 
+ * to be found here: http://orpara.com/acedbdriver/index.html. 
+ * 
+ * Pól Ua Laoínecháin has actually used the AceDB server system recently, 
+ * so may also be of assistance.
+ *
+ * 
+ * Kemin Zhou can be contacted here: kmzhou4@yahoo.com
+ * and 
+ * Pól Ua Laoínecháin can be contacted here: linehanp@tcd.ie.
+ *
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published here: http://www.gnu.org/licenses/agpl-3.0.html and
+ * explained here: https://www.gnu.org/licenses/why-affero-gpl.html
+ * as published by the Free Software Foundation. 
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, write to the Free Software Foundation, Inc., 
+ * 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package com.relvar.ACeDB2Java;
+
+public class ACeDB2JavaObjectNotFoundException extends ACeDB2JavaException
+{
+  ACeDB2JavaObjectNotFoundException(String s)
+  {
+    super(s);
+  }
+}
